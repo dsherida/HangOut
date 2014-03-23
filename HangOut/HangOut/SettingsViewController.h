7aae68e6ca1d4aae61e1eae7c7c46e90763690eb
Ian Ferreira dos Santos <ianfs@ians-mbp.mobile.asu.edu>
2014-03-22 19:40:06 -0700

    solved logging out issue, modified tab names, created new settings view controller

---
+++ HangOut/HangOut/SettingsViewController.h
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.h
+//  HangOut
+//
+//  Created by Ian Ferreira dos Santos on 3/22/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+#import <FacebookSDK/FacebookSDK.h>
+
+@interface SettingsViewController : UIViewController
+
+- (IBAction)logOutButtonPressed:(UIButton *)sender;
+- (IBAction)okButtonPressed:(UIButton *)sender;
+
+@end

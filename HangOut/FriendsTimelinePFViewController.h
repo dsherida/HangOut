eb4289e35075fa3abcd28238e8b911f238015ff1
Ian Ferreira dos Santos <ianfs@ians-mbp.mobile.asu.edu>
2014-03-24 22:09:38 -0700

    Timeline is surfacing! YAY

---
+++ HangOut/FriendsTimelinePFViewController.h
@@ -0,0 +1,17 @@
+//
+//  FriendsTimelinePFViewController.h
+//  HangOut
+//
+//  Created by Ian Ferreira dos Santos on 3/24/14.
+//
+//
+
+#import <Parse/Parse.h>
+
+@interface FriendsTimelinePFViewController : PFQueryTableViewController
+
+@property (nonatomic,weak) NSString *wishTitle;
+@property (nonatomic,weak) NSString *message;
+@property (nonatomic,weak) NSString *profilePic;
+
+@end

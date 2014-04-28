92d22d86559273aa7ecce5c2a50f4adeebd02726
dsherida <dsherida@asu.edu>
2014-04-28 14:59:31 -0700

    profile page fixed, added comment and attendees button

--- HangOut/HangOut/ProfileViewController.m
+++ HangOut/HangOut/ProfileViewController.m
@@ -30,9 +30,6 @@ int tableSize;
 //        self.message = @"info";
 //        self.profilePic = @"User";
         
-        // Uncomment the following line to specify the key of a PFFile on the PFObject to display in the imageView of the default cell style
-        // self.imageKey = @"image";
-        
         // Whether the built-in pull-to-refresh is enabled
         self.pullToRefreshEnabled = YES;
         
@@ -54,37 +51,11 @@ int tableSize;
 {
     [super viewDidLoad];
     NSLog(@"Profile view did load");
-//    userModel = [UserModel sharedUserModel];
-    
-    //    [userModel getAndSetWishArray];
-    //    tableSize = [userModel.wishArray count] + 1;
-    
-    //NSLog(@"Loaded Profile View");
-    //NSLog(@"Username: %@", userModel.userName);
-    
-    //[self.tableView reloadData];
-    
-    //UIImage *image = [UIImage imageWithData:userModel.profilePictureData];
-    //[_UIProfilePic setImage:image];
-    
-    //[_UINameLabel setText:userModel.userName];
-    //UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:url]]];
-    
-    //PFUser *user = [PFUser currentUser];
-    //self.userName.text = user.username;
-    
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)viewDidUnload {
     [super viewDidUnload];
     // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -124,29 +95,6 @@ int tableSize;
     // This method is called every time objects are loaded from Parse via the PFQuery
 }
 
-// Override to customize what kind of query to perform on the class. The default is to query for
-// all objects ordered by createdAt descending.
-- (PFQuery *)queryForTable {
-    
-    PFQuery *queryWish = [PFQuery queryWithClassName:kWishClassKey];
-    [queryWish whereKey:@"User" equalTo:[PFUser currentUser]];
-    
-    // If Pull To Refresh is enabled, query against the network by default.
-    if (self.pullToRefreshEnabled) {
-        queryWish.cachePolicy = kPFCachePolicyNetworkOnly;
-    }
-    
-    // If no objects are loaded in memory, we look to the cache first to fill the table
-    // and then subsequently do a query against the network.
-    if (self.objects.count == 0) {
-        queryWish.cachePolicy = kPFCachePolicyCacheThenNetwork;
-    }
-    
-    [queryWish orderByDescending:@"createdAt"];
-    
-    return queryWish;
-}
-
 #pragma mark - Table view data source
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -180,123 +128,131 @@ int tableSize;
     return nil;
 }
 
+// Override to customize what kind of query to perform on the class. The default is to query for
+// all objects ordered by createdAt descending.
+- (PFQuery *)queryForTable {
+    NSLog(@"Querying for table...");
+    PFQuery *queryWish;
+    
+    if ([PFUser currentUser] != nil) {
+        queryWish = [PFQuery queryWithClassName:kWishClassKey];
+        [queryWish whereKey:@"User" equalTo:[PFUser currentUser]];
+        
+        // If Pull To Refresh is enabled, query against the network by default.
+        if (self.pullToRefreshEnabled) {
+            queryWish.cachePolicy = kPFCachePolicyNetworkOnly;
+        }
+        
+        // If no objects are loaded in memory, we look to the cache first to fill the table
+        // and then subsequently do a query against the network.
+        if (self.objects.count == 0) {
+            queryWish.cachePolicy = kPFCachePolicyCacheThenNetwork;
+        }
+        
+        [queryWish orderByDescending:@"createdAt"];
+    }
+    
+    return queryWish;
+}
 
 // Override to customize the look of a cell representing an object. The default is to display
 // a UITableViewCellStyleDefault style cell with the label being the textKey in the object,
 // and the imageView being the imageKey in the object.
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath object:(PFObject *)object {
-    NSLog(@"Building table view...");
+//    NSLog(@"Building table view...");
     static NSString *profileCellIdentifier = @"profileCell";
     static NSString *wishCellIdentifier = @"wishCell";
 
     PFTableViewCell *profileCell = (PFTableViewCell *)[tableView dequeueReusableCellWithIdentifier:profileCellIdentifier];
     PFTableViewCell *wishCell = (PFTableViewCell *)[tableView dequeueReusableCellWithIdentifier:wishCellIdentifier];
     
-    if (profileCell == nil) {
-        profileCell = [[PFTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:profileCellIdentifier];
-    }
+    if ([PFUser currentUser] != nil) {
+        if (profileCell == nil) {
+            profileCell = [[PFTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:profileCellIdentifier];
+        }
+        
+        if (wishCell == nil) {
+            wishCell = [[PFTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:wishCellIdentifier];
+        }
+        
+        // If we are looking at the first row
+        if (indexPath.row == 0) {
+            // Configure the profileCell
+            UIImageView *profileImage = (UIImageView *)[profileCell viewWithTag:1];
+            [profileImage setImage:userModel.profileUIImage];
+            
+            UILabel *userName = (UILabel *)[profileCell viewWithTag:2];
+            userName.text = userModel.userName;
+            
+            return profileCell;
+        } else  {
+            // Configure wishCell
+//            NSLog(@"indexPath row: %ld", (long)indexPath.row);
+            
+            UILabel *wishLabel = (UILabel *)[wishCell viewWithTag:3];
+            wishLabel.text = [object objectForKey:@"title"];
+            
+            HangOutDetailsButton *comments = (HangOutDetailsButton *)[wishCell viewWithTag:4];
+            comments.object = object;
+            
+            HangOutDetailsButton *attendees = (HangOutDetailsButton *)[wishCell viewWithTag:5];
+            attendees.object = object;
+            
+            PFQuery *attendeesQuery = [PFQuery queryWithClassName:kActivityClassKey];
+            [attendeesQuery whereKey:@"type" equalTo:@"going"];
+            if (object != nil)
+                [attendeesQuery whereKey:@"wish" equalTo:object];
     
-    if (wishCell == nil) {
-        wishCell = [[PFTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:wishCellIdentifier];
-    }
-    
-    // If we are looking at the first row
-    if (indexPath.row == 0) {
-        // Configure the profileCell
-        UIImageView *profileImage = (UIImageView *)[profileCell viewWithTag:1];
-        [profileImage setImage:userModel.profileUIImage];
-        
-        UILabel *userName = (UILabel *)[profileCell viewWithTag:2];
-        userName.text = userModel.userName;
-        
-//        self.object = [object objectForKey:@"title"];
-        //NSLog(@"%@", self.object);
-//        for(id key in self.object)
-//            NSLog(@"key=%@ value=%@", key, [self.object objectForKey:key]);
-        
-        return profileCell;
-    } else  { //if (indexPath.row > 0)
-        // Configure wishCell
-        NSLog(@"indexPath row: %ld", (long)indexPath.row);
-        
-        UILabel *wishLabel = (UILabel *)[wishCell viewWithTag:3];
-        wishLabel.text = [object objectForKey:@"title"];
-        return wishCell;
+            [attendeesQuery countObjectsInBackgroundWithBlock:^(int number, NSError *error) {
+                [attendees setTitle:[NSString stringWithFormat:@"%d", number] forState:UIControlStateNormal];
+            }];
+            
+            PFQuery *commentsQuery = [PFQuery queryWithClassName:kActivityClassKey];
+            [commentsQuery whereKey:@"type" equalTo:@"comment"];
+            if (object != nil)
+                [commentsQuery whereKey:@"wish" equalTo:object];
+            
+            [commentsQuery countObjectsInBackgroundWithBlock:^(int number, NSError *error) {
+                [comments setTitle:[NSString stringWithFormat:@"%d", number] forState:UIControlStateNormal];
+            }];
+            
+            return wishCell;
+        }
     }
     
     return nil;
 }
 
-
-
-
-//- (id)initWithStyle:(UITableViewStyle)style {
-//    self = [super initWithStyle:style];
-//    if (self) {
-//        // Custom initialization
-//    }
-//    return self;
-//}
-
-
-
-
-
-
-
-
-
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+- (IBAction)detailsButtonClicked:(id)sender {
+    NSLog(@"detailsButton was clicked!");
+    
+    // -- begin reference:
+    // Open source code from: http://stackoverflow.com/questions/7504421/getting-row-of-uitableview-cell-on-button-press
+    CGPoint buttonPosition = [sender convertPoint:CGPointZero toView:self.tableView];
+    NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:buttonPosition];
+    // -- end reference
+    
+    // -- begin reference:
+    // open source code from: http://stackoverflow.com/questions/2384435/how-can-i-get-a-uitableviewcell-by-indexpath
+    UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
+    // -- end reference
+    
+    UILabel *title = (UILabel *)[cell viewWithTag:3];
+    NSString *wishTitle = title.text;
+    NSLog(@"Wish title: %@", wishTitle);
 }
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
-- (IBAction)wishListElementClicked:(UIButton *)sender {
+- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([segue.identifier isEqualToString:@"Attendees"]) {
+        ListOfAttendeesPFViewController *list = [segue destinationViewController];
+        HangOutDetailsButton *button = (HangOutDetailsButton*) sender;
+        list.object = button.object;
+    }
+    if ([segue.identifier isEqualToString:@"Comments"]) {
+        CommentsPFViewController *list = [segue destinationViewController];
+        HangOutDetailsButton *button = (HangOutDetailsButton*) sender;
+        list.object = button.object;
+    }
 }
 
 @end
